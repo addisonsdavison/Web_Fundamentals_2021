08b22a9e5ff0b7b4fadcd0429e750411c5185091
addisonsdavison <addisondavison@hotmail.com>
2021-06-23 19:50:17 -0700

    master

---
+++ algos.js
@@ -0,0 +1,15 @@
+
+// Given an Array print amount of positive numbers in Array
+
+var countPositives = 0;
+var numbers = [3, 4, -2, 7, 16, -8, 0];
+
+for(var i = 0; i <= numbers.length; i++){
+    if(numbers[i] >= 0){
+        countPositives += 1;
+    }
+}
+    
+// your code here!
+    
+console.log("there are " + countPositives + " positive values");
